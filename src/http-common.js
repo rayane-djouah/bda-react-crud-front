0cb287be8794b3c8b45f4fc13fd1df2dec723d72
Rayane Djouah <77965000+rayane-djouah@users.noreply.github.com>
2024-05-15 23:13:10 +0100

    fix

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://2a86-105-103-131-130.ngrok-free.app/",
+  baseURL: "https://b0f0-105-103-131-130.ngrok-free.app//",
   headers: {
     "Content-type": "application/json",
     "ngrok-skip-browser-warning": "true",
